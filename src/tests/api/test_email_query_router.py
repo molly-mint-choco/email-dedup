ba924b27f756ffdf92ffac145d0a118e4d42a62f
molly <89737494+molly-mint-choco@users.noreply.github.com>
2026-01-04 23:03:15 +0800

    update unit test for router

--- src/tests/api/test_email_query_router.py
+++ src/tests/api/test_email_query_router.py
@@ -8,7 +8,8 @@ from app.api.routers import email_query_router
 import pytest_asyncio
 
 MOCK_UUID = uuid.uuid4()
-MOCK_FILE_NAME = "doc1234.txt"
+MOCK_FILE_NAME1 = "doc1234.txt"
+MOCK_FILE_NAME2 = "doc4567.txt"
 
 app = FastAPI()
 app.include_router(router)
@@ -31,10 +32,42 @@ async def test_get_canonical_id_by_document_success(client, mocker):
     )
 
     # Act
-    response = await client.get(f"/emails/document/{MOCK_FILE_NAME}/canonical-id")
+    response = await client.get(f"/emails/document/{MOCK_FILE_NAME1}/canonical-id")
     
     
     # Assert
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == str(MOCK_UUID)
+
+@pytest.mark.asyncio
+async def test_get_canonical_id_not_found(client, mocker):
+    # Arrange
+    mocker.patch.object(
+        email_query_router.query_handler,
+        'get_canonical_id_by_document_async',
+        new=mocker.AsyncMock(return_value=None)
+    )
     
+    # Act
+    response = await client.get(f"/emails/document/{MOCK_FILE_NAME1}/canonical-id")
+    
+    # Assert
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert f"Document '{MOCK_FILE_NAME1}' not found" in response.json()["detail"]
+
+@pytest.mark.asyncio
+async def test_get_documents_by_thread_success(client, mocker):
+    # Arrange
+    documents = [MOCK_FILE_NAME1, MOCK_FILE_NAME2]
+    mock_method = mocker.patch.object(
+        email_query_router.query_handler,
+        'get_documents_by_thread_async',
+        new=mocker.AsyncMock(return_value=documents)
+    )
+    
+    # Act
+    response = await client.get(f"/emails/canonical-thread/{MOCK_UUID}/documents")
+    
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == documents
